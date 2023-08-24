03f2f0b9b520038ad54cdaf0cbf9a94c8dd27b4f
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-08-23 21:14:37 -0400

    fix

--- tokenization.c
+++ tokenization.c
@@ -15,23 +15,23 @@
 char **tokenization(char *ptr, char *delim)
 {
     char *token = NULL, **tokens = NULL;
-    int i = 0;
+    int i = 0, j;
 
     tokens = malloc(sizeof(char *) * 10);
     token = strtok(ptr, delim);
 
     while (token)
     {
-        tokens[i] = malloc(sizeof(char) * (_strlen(token) + 1));
+        tokens[i] = malloc(sizeof(char) * (strlen(token) + 1));
         if (tokens[i] == NULL)
         {
-            // Handle allocation failure
-            for (int j = 0; j < i; j++)
+            
+            for (j = 0; j < i; j++)
                 free(tokens[j]);
             free(tokens);
             return NULL;
         }
-        _strcpy(tokens[i], token);
+        strcpy(tokens[i], token);
         i++;
         token = NULL;
         token = strtok(NULL, delim);
@@ -39,8 +39,7 @@ char **tokenization(char *ptr, char *delim)
 
     tokens[i] = NULL;
 
-    // Free memory for each token before freeing the tokens array
-    for (int j = 0; j < i; j++)
+    for (j = 0; j < i; j++)
         free(tokens[j]);
 
     free(token);
